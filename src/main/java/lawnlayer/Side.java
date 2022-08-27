9f5e0e368cb9f18ae5df480396d3945f4cff9b9b
dnngky <dan.kynguyen2@outlook.com>
2022-08-27 14:50:56 +1000

    Delete gradle

--- src/main/java/lawnlayer/Side.java
+++
@@ -1,9 +0,0 @@
-package lawnlayer;
-
-/**
- * An enum indicating the sides of collision of Enemy types.
- */
-public enum Side {
-    
-    TOP, BOTTOM, LEFT, RIGHT, TOPLEFT, BOTTOMLEFT, TOPRIGHT, BOTTOMRIGHT, NONE;
-}
